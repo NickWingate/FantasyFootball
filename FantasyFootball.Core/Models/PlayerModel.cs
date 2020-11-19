9c4f74d69a964146c789c8848cae3d5944782793
Nick Wingate <nicholas.wingate03@gmail.com>
2020-11-19 22:07:19 +0000

    Added constructor for PlayerModel

--- FantasyFootball.Core/Models/PlayerModel.cs
+++ FantasyFootball.Core/Models/PlayerModel.cs
@@ -11,5 +11,13 @@ namespace FantasyFootball.Core.Models
         public int YellowCards { get; set; }
         public int RedCards { get; set; }
         public int Value => 10 * Goals + -2 * YellowCards + -5 * RedCards;
+
+        public PlayerModel(string name, int goals, int yCards, int rCards)
+        {
+            Name = name;
+            Goals = goals;
+            YellowCards = yCards;
+            RedCards = rCards;
+        }
     }
 }
