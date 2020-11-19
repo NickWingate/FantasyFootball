bf4fe792baafc9e294cd7d5eaa96759f679fcb06
Nick Wingate <nicholas.wingate03@gmail.com>
2020-11-19 18:17:59 +0000

    Created TeamModel. Holds 5 Players

---
+++ FantasyFootball.Core/Models/TeamModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FantasyFootball.Core.Models
+{
+    public class TeamModel
+    {
+        public string Name { get; set; }
+        public List<PlayerModel> Players { get; set; }
+        public int Size => Players.Count;
+        public int Score
+        {
+            get
+            {
+                int total = 0;
+                foreach (PlayerModel player in Players)
+                {
+                    total += player.Value;
+                }
+                return total;
+            }
+        }
+    }
+}
