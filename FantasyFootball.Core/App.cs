de1ddde9b20b470f71e5d3d30e0800f67b2c03be
Nick Wingate <nicholas.wingate03@gmail.com>
2020-11-25 01:19:53 +0000

    Added main menu
    Named Menu(View/ViewModel).
    Need to implement TeamName logic.

--- FantasyFootball.Core/App.cs
+++ FantasyFootball.Core/App.cs
@@ -7,7 +7,7 @@ namespace FantasyFootball.Core
     {
         public override void Initialize()
         {
-            RegisterAppStart<TeamViewModel>();
+            RegisterAppStart<MenuViewModel>();
         }
     }
 }
---
+++ FantasyFootball.Core/ViewModels/MenuViewModel.cs
@@ -0,0 +1,38 @@
+﻿using MvvmCross.Commands;
+using MvvmCross.Navigation;
+using MvvmCross.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace FantasyFootball.Core.ViewModels
+{
+    public class MenuViewModel : MvxViewModel
+    {
+        private readonly IMvxNavigationService _navigationService;
+        public MenuViewModel(IMvxNavigationService navigationService)
+        {
+            _navigationService = navigationService;
+            NewTeamCommand = new MvxCommand(NewTeam);
+            LoadTeamCommand = new MvxCommand(LoadTeam);
+        }
+        public IMvxCommand NewTeamCommand { get; set; }
+        public IMvxCommand LoadTeamCommand { get; set; }
+
+        private void NewTeam()
+        {
+            Debug.WriteLine("New Team Button Clicked");
+            _navigationService.Navigate<TeamNameViewModel>();
+            // Get Team Name
+            // Pass into TeamViewModel
+        }
+        private void LoadTeam()
+        {
+            Debug.WriteLine("Load Team Button Clicked");
+            // Open File
+            //
+            // Pass into TeamViewModel
+        }
+    }
+}
